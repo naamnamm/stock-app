4650a5895490afa4af4267be9318fd0043fd4fb0
Naam <naam.pt@gmail.com>
2020-12-20 09:24:52 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.listen(port, () => console.log(`server started on port ${port}`));
