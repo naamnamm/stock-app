edc5db73d7049abc2cb537370213a8288f305f13
Naam <naam.pt@gmail.com>
2021-01-14 11:50:51 -0500

    installed dotenv, connected to the db and set up login process

--- client/src/components/login-signup/Login.js
+++ client/src/components/login-signup/Login.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Form, Button, Navbar, Nav, Col, Row } from 'react-bootstrap';
 import './Login-Signup.css';
 
 const Login = ({ handleLogin }) => {
+  const [errorMsg, setErrorMsg] = useState('');
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = {
+        username: usernameRef.current.value.toLowerCase(),
+        password: passwordRef.current.value,
+      };
+
+      const config = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await fetch('/login', config);
+      const loginData = await response.json();
+
+      if (!response.ok) {
+        setErrorMsg(loginData.error.message);
+      } else {
+        // localStorage.setItem('accessToken', JSON.stringify(loginData.token));
+        handleLogin(true);
+        //onUserSubmit(loginData);
+        //setAuth(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Navbar bg='light' variant='light'>
@@ -14,13 +51,17 @@ const Login = ({ handleLogin }) => {
         </Nav>
       </Navbar>
       <div className='login-container mx-auto mt-4'>
-        <Form>
+        <Form onSubmit={handleLoginSubmit}>
           <Form.Group as={Row} controlId='formHorizontalEmail'>
             <Form.Label column sm={3} className='text-left'>
-              Email
+              Username
             </Form.Label>
             <Col sm={9}>
-              <Form.Control type='email' placeholder='Email' />
+              <Form.Control
+                type='username'
+                placeholder='Username'
+                ref={usernameRef}
+              />
             </Col>
           </Form.Group>
 
@@ -29,12 +70,15 @@ const Login = ({ handleLogin }) => {
               Password
             </Form.Label>
             <Col sm={9}>
-              <Form.Control type='password' placeholder='Password' />
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                ref={passwordRef}
+              />
             </Col>
           </Form.Group>
-          <Button type='submit' onClick={() => handleLogin(true)}>
-            Login
-          </Button>
+          {errorMsg ? <div className='text-danger'>{errorMsg}</div> : null}
+          <Button type='submit'>Login</Button>
           <Form.Text id='passwordHelpBlock' muted className='mt-3'>
             Don't have an account
           </Form.Text>
--- client/src/components/login-signup/Signup.js
+++ client/src/components/login-signup/Signup.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Form, Button, Col, Navbar, Nav } from 'react-bootstrap';
 import './Login-Signup.css';
 
 const Signup = ({ handleSignup }) => {
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = {
+        username: usernameRef.current.value.toLowerCase(),
+        password: passwordRef.current.value,
+      };
+
+      const config = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await fetch('/signup', config);
+      console.log(response);
+      const signupData = await response.json();
+      console.log(signupData);
+
+      !response.ok ? setErrorMsg(signupData) : handleSignup(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Navbar bg='light' variant='light'>
@@ -14,18 +46,34 @@ const Signup = ({ handleSignup }) => {
         </Nav>
       </Navbar>
       <div className='signup-container mx-auto'>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Row>
             <Form.Group as={Col} controlId='formGridEmail'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' placeholder='Enter email' />
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type='username'
+                placeholder='Enter Username'
+                ref={usernameRef}
+                required
+              />
             </Form.Group>
 
             <Form.Group as={Col} controlId='formGridPassword'>
               <Form.Label>Password</Form.Label>
-              <Form.Control type='password' placeholder='Password' />
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                ref={passwordRef}
+                required
+              />
             </Form.Group>
           </Form.Row>
+
+          {errorMsg
+            ? errorMsg.map((err) => (
+                <div className='text-danger'>{err.message}</div>
+              ))
+            : null}
 
           <Form.Group controlId='formGridAddress1'>
             <Form.Label>Address</Form.Label>
@@ -61,11 +109,7 @@ const Signup = ({ handleSignup }) => {
             <Form.Check type='checkbox' label='Agree to our terms' />
           </Form.Group>
 
-          <Button
-            variant='primary'
-            type='submit'
-            onClick={() => handleSignup(true)}
-          >
+          <Button variant='primary' type='submit'>
             Signup
           </Button>
         </Form>
