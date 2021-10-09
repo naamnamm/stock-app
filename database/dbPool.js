d304cc7ad73dca14958cc9bafb366749b4acc570
Naam <naam.pt@gmail.com>
2021-10-09 08:34:08 -0400

    update dbpool to connect to the cloud db

--- database/dbPool.js
+++ database/dbPool.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_DATABASE,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  user: 'qxzmoxxb',
+  password: 'TbV_UouGemb2PbhBExheE8na8LRKS9oN',
+  host: 'lallah.db.elephantsql.com',
+  port: 5432,
+  database: 'qxzmoxxb',
 });
 
 module.exports = pool;
