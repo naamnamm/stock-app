b2365b66cd6edbbe63708527693edfb7d6949ead
Naam <naam.pt@gmail.com>
2021-11-21 07:41:02 -0500

    update chart.js

--- utils/chart.js
+++ utils/chart.js
@@ -1,38 +1,52 @@
 const moment = require('moment');
 
-Array.prototype.move = function (from, to) {
-  this.splice(to, 0, this.splice(from, 1)[0]);
-};
+// Array.prototype.move = function (from, to) {
+//   this.splice(to, 0, this.splice(from, 1)[0]);
+// };
 
+// function formatChart(fetchObject) {
+//   const labelXandY = ['date', 'close'];
+//   const output = fetchObject.chart.map((obj) => {
+//     return Object.entries(obj)
+//       .filter((item) => {
+//         if (labelXandY.includes(item[0])) {
+//           return item;
+//         }
+//       })
+//       .flat()
+//       .filter((item, index) => {
+//         return index % 2 != 0;
+//       })
+//       .map((item) => {
+//         //debugger;
+//         if (item.length === 10 && moment(item, 'YYYY/MM/DD').isValid()) {
+//           return moment(item).format('MMM DD');
+//         }
+//         return item;
+//       });
+//   });
+
+//   output.map((item) => {
+//     item.move(0, 1);
+//     return item;
+//   });
+
+//   output.unshift(labelXandY);
+//   return output;
+// }
+
+// create a function to format date
+const formatDate = (date) => moment(date).format('MMM DD');
+
+// manipulate the object to get chart data
 function formatChart(fetchObject) {
-  const labelXandY = ['date', 'close'];
-  const output = fetchObject.chart.map((obj) => {
-    return Object.entries(obj)
-      .filter((item) => {
-        if (labelXandY.includes(item[0])) {
-          return item;
-        }
-      })
-      .flat()
-      .filter((item, index) => {
-        return index % 2 != 0;
-      })
-      .map((item) => {
-        //debugger;
-        if (item.length === 10 && moment(item, 'YYYY/MM/DD').isValid()) {
-          return moment(item).format('MMM DD');
-        }
-        return item;
-      });
+  const formattedData = [['date', 'close']];
+
+  fetchObject.chart.forEach(({ date, close }) => {
+    formattedData.push([formatDate(date), close]);
   });
 
-  output.map((item) => {
-    item.move(0, 1);
-    return item;
-  });
-
-  output.unshift(labelXandY);
-  return output;
+  return formattedData;
 }
 
 module.exports = formatChart;
