ea9bf24db2eb68093d95af4ef8235050f28d3225
Naam Pondpat <naam.pt@gmail.com>
2023-03-13 11:13:13 -0400

    update db script

--- database/db.sql
+++ database/db.sql
@@ -9,22 +9,13 @@ CREATE TABLE users (
   last_active_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (name, password) VALUES ('ben', '123');
-SELECT * FROM users;
-
 CREATE TABLE cash_transfer (
-  id TEXT PRIMARY KEY DEFAULT generate_uuid(5),
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   type TEXT NOT NULL,
   amount NUMERIC(8,2) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   user_id uuid REFERENCES users(id)
 );
-
--- to trencate cash_transfer 
-
-ALTER TABLE cash_transfer ALTER COLUMN id DROP DEFAULT, 
-ALTER COLUMN id SET DATA TYPE UUID USING (uuid_generate_v4()), 
-ALTER COLUMN id SET DEFAULT uuid_generate_v4();
 
 CREATE TABLE watchlists (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -32,7 +23,6 @@ CREATE TABLE watchlists (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   user_id uuid REFERENCES users(id)
 );
-
 
 CREATE TABLE orders (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -53,18 +43,6 @@ CREATE TABLE currentHoldings (
   user_id uuid REFERENCES users(id)
 );
 
-CREATE UNIQUE INDEX currentHoldings_user_id ON currentHoldings (user_id, symbol);
-
--- old table
-CREATE TABLE cash_balance (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  type TEXT NOT NULL,
-  amount NUMERIC(8,2) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  user_id uuid REFERENCES users(id)
-);
-
--- update cash balance table
 CREATE TABLE cash_balances (
   id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   amount NUMERIC(8,2) NOT NULL,
@@ -72,7 +50,9 @@ CREATE TABLE cash_balances (
   user_id uuid REFERENCES users(id)
 );
 
+CREATE UNIQUE INDEX currentHoldings_user_id ON currentHoldings (user_id, symbol);
 CREATE UNIQUE INDEX cash_balances_user_id ON cash_balances (user_id);
 
-
+INSERT INTO users (name, password) VALUES ('ben', '123');
+SELECT * FROM users;
 
